6c6a0b42d1353be200d36194816bfeb770fd2985
Conor Casey <conorcaseyc@icloud.com>
2021-03-30 00:43:19 +0100

    Added pressure surfaces

--- gfs.py
+++ gfs.py
@@ -9,12 +9,10 @@ from astropy import units
 import sys
 
 # Define preliminary urls.
-# 2-m temperature.
-tmp_url = "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t{}z.pgrb2.1p00.f{}&lev_2_m_above_ground=on&var_TMP=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.{}"
-# 850 hPa temperature.
-t850_url = "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t{}z.pgrb2.1p00.f{}&lev_850_mb=on&var_TMP=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.{}"
-# 500 hPa geopotential height.
-z500_url = "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t{}z.pgrb2.1p00.f{}&lev_500_mb=on&var_HGT=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.{}"
+# Air temperature.
+t_url = "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t{}z.pgrb2.1p00.f{}&lev_1000_mb=on&lev_100_mb=on&lev_15_mb=on&lev_200_mb=on&lev_250_mb=on&lev_300_mb=on&lev_400_mb=on&lev_500_mb=on&lev_50_mb=on&lev_600_mb=on&lev_700_mb=on&lev_850_mb=on&lev_925_mb=on&var_TMP=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.{}"
+# Geopotential height.
+z_url = "https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t{}z.pgrb2.1p00.f{}&lev_1000_mb=on&lev_100_mb=on&lev_15_mb=on&lev_200_mb=on&lev_250_mb=on&lev_300_mb=on&lev_400_mb=on&lev_500_mb=on&lev_50_mb=on&lev_600_mb=on&lev_700_mb=on&lev_850_mb=on&lev_925_mb=on&var_HGT=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.{}"
 
 # Retrieve date.
 date = datetime.now()
@@ -38,6 +36,7 @@ def download_var(href):
     url = href.format(
         forecast_hour, hour, str_date
     ) + forecast_hour
+    url += "%2Fatmos"
 
     # Download file.
     file = wget.download(url, bar=None)
@@ -60,27 +59,19 @@ def download_var(href):
 
 # Download.
 # Progress bar.
-t = tqdm(total=3, desc='Downloading')
+pbar = tqdm(total=2, desc='Downloading')
 
-# 2-m temperature.
-tmp = download_var(tmp_url)
-tmp.standard_name = None
-tmp.long_name = '2m_temperature'
-tmp.var_name = 't2m'
-iris.save(tmp, 'gfs/t2m.nc')
-t.update()
+# Air temperature.
+t = download_var(t_url)
+t.var_name = 't'
+iris.save(t, 'gfs/t.nc')
+pbar.update()
 
-# 850 hPa temperature.
-t850 = download_var(t850_url)
-t850.var_name = 't'
-iris.save(t850, 'gfs/t.nc')
-t.update()
-
-# 500 hPa geopotential height.
+# Geopotential.
 g = 9.80665
-z500 = download_var(z500_url) * g
-z500.units = 'm2 s-2'
-z500.standard_name = 'geopotential'
-z500.var_name = 'z'
-iris.save(z500, 'gfs/z.nc')
-t.update()
+z = download_var(z_url) * g
+z.units = 'm2 s-2'
+z.standard_name = 'geopotential'
+z.var_name = 'z'
+iris.save(z, 'gfs/z.nc')
+pbar.update()
